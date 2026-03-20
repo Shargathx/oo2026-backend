25a8c4fc9dae1bc88094cd5a97653dbb02acb7ea
Martin Saareväli <martin.saarevali@hotmail.com>
2026-03-20 13:35:54 +0200

    Delete proovikontrolltoo directory

--- proovikontrolltoo/src/main/java/ee/msaareva/proovikontrolltoo/entity/FilmType.java
+++
@@ -1,7 +0,0 @@
-package ee.msaareva.proovikontrolltoo.entity;
-
-public enum FilmType {
-    NEW_RELEASE,
-    REGULAR_RELEASE,
-    OLD_RELEASE,
-}
--- proovikontrolltoo/src/main/java/ee/msaareva/proovikontrolltoo/repository/FilmRepository.java
+++
@@ -1,8 +0,0 @@
-package ee.msaareva.proovikontrolltoo.repository;
-
-import ee.msaareva.proovikontrolltoo.entity.Film;
-import lombok.NonNull;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmRepository extends JpaRepository<@NonNull Film, @NonNull Long> {
-}
