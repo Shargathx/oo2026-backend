dc914e27892775c1df92da40ac07b5f7eeee5143
Martin Saareväli <martin.saarevali@hotmail.com>
2026-03-20 13:35:47 +0200

    Delete kontrolltoo directory

--- kontrolltoo/src/main/java/ee/msaareva/kontrolltoo/repository/LetterCounterRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.msaareva.kontrolltoo.repository;
-
-import ee.msaareva.kontrolltoo.entity.LetterCounter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LetterCounterRepository extends JpaRepository<LetterCounter, Long> {
-}
--- kontrolltoo/src/main/java/ee/msaareva/kontrolltoo/repository/WordRepository.java
+++
@@ -1,8 +0,0 @@
-package ee.msaareva.kontrolltoo.repository;
-
-import ee.msaareva.kontrolltoo.entity.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordRepository extends JpaRepository<Word, Long> {
-    boolean existsByWord(String word);
-}
